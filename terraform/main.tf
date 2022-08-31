54edfd70492c82dca63fe7db4a179e27cd847db1
Marcus Irani <marcusirani@Marcuss-MacBook-Pro.local>
2022-08-30 19:56:32 -0700

    Adding todolist app

--- terraform/main.tf
+++ terraform/main.tf
@@ -95,3 +95,16 @@ resource "kubectl_manifest" "my-nginx-app" {
     yaml_body = element(data.kubectl_file_documents.my-nginx-app.documents, count.index)
     override_namespace = "argocd"
 }
+
+data "kubectl_file_documents" "my-todolist-app" {
+  content = file("../manifests/argocd/my-todolist-app.yaml")
+}
+
+resource "kubectl_manifest" "my-nginx-app" {
+  depends_on = [
+    kubectl_manifest.argocd,
+  ]
+  count     = length(data.kubectl_file_documents.my-todolist-app.documents)
+  yaml_body = element(data.kubectl_file_documents.my-todolist-app.documents, count.index)
+  override_namespace = "argocd"
+}
